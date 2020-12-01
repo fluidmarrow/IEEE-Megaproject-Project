e305ba138734bcb9084d7cbda9bd588d1f8c1404
abhirishi0805 <abhirishi0805@gmail.com>
2020-12-01 20:21:58 +0530

    OTP Service Added

--- app/src/main/java/com/example/e_fir/MainActivity.java
+++ app/src/main/java/com/example/e_fir/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.e_fir;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
@@ -8,6 +9,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
@@ -15,14 +17,25 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.toolbox.HttpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskExecutors;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
     TextView tvHeading;
     EditText etMobile, etOTP;
     CardView cvOTP;
-    int otp;
+    String mobile, verificationCodeByUser, verificationCodeBySystem;
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,30 +58,44 @@ public class MainActivity extends AppCompatActivity {
         cvOTP.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 if (etMobile.getText().toString().trim().isEmpty()) {
                     Toast.makeText(MainActivity.this, "Enter your mobile number", Toast.LENGTH_SHORT).show();
                 }
                 else{
-                    try {
-                        String api="AolHa8PNKw7ZGRWpDJYSUCz3FQ4Eq5ue1v2cfO9BgdbysjitxTbSL9Vy32KF7ZQXINDH4Ocahj8mnfGC";
-                        otp = new Random().nextInt(8998) ;
-//                        HttpResponse response = Unirest.post("https://www.fast2sms.com/dev/bulk")
-//                                .header("authorization", "YOUR_API_KEY")
-//                                .header("cache-control", "no-cache")
-//                                .header("content-type", "application/x-www-form-urlencoded")
-//                                .body("sender_id=FSTSMS&language=english&route=qt&numbers=etMobile.getText().toString()&message=39599&variables={#AA#}&variables_values=otp")
-//                                .asString();
-                        Toast.makeText(MainActivity.this, "OTP Successfully Sent", Toast.LENGTH_SHORT).show();
-                        showDialog();
-                    }
-                    catch (Exception e)
-                    {
-                        Toast.makeText(MainActivity.this, "Error Occurred" , Toast.LENGTH_LONG).show();
-                    }
+                    mobile = etMobile.getText().toString().trim();
+                    sendOTP();
                 }
             }
         });
+    }
+
+    private void sendOTP() {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                "+91" + mobile,           //Phone number to verify
+                60,                       //Timeout duration
+                TimeUnit.SECONDS,
+                MainActivity.this,                //Activity (for callback binding)
+                new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                    @Override
+                    public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                        signInUser(phoneAuthCredential);
+                    }
+
+                    @Override
+                    public void onVerificationFailed(@NonNull FirebaseException e) {
+                        Toast.makeText(MainActivity.this, "Verification Failed", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, "onVerificationFailed: " + e.getLocalizedMessage());
+                    }
+
+                    @Override
+                    public void onCodeSent(@NonNull String verificationID, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                        super.onCodeSent(verificationID, forceResendingToken);
+
+                        verificationCodeBySystem = verificationID;
+                        Toast.makeText(MainActivity.this, "OTP Successfully Sent", Toast.LENGTH_SHORT).show();
+                        showDialog();
+                    }
+                });                 //OnVerificationStateChangedCallbacks
     }
 
 
@@ -105,39 +132,46 @@ public class MainActivity extends AppCompatActivity {
         dialogBuilder.setPositiveButton("Validate", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+                verificationCodeByUser = etOTP.getText().toString().trim();
 
-                int OTP = Integer.parseInt(etOTP.getText().toString().trim());
-                if(otp==OTP){
-                    Intent intent = new Intent(MainActivity.this, registerFIR.class);
-                    intent.putExtra("mobile", etMobile.getText().toString().trim());
-                    startActivity(intent);}
+                if(verificationCodeByUser.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please enter the OTP", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationCodeBySystem, verificationCodeByUser);
+                    signInUser(credential);
+                }
             }
         })
                 .setNegativeButton("Resend", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-
-                        try {
-                            // Construct data
-                            otp = new Random().nextInt(8998) ;
-//                            HttpResponse response = Unirest.post("https://www.fast2sms.com/dev/bulk")
-//                                    .header("authorization", "YOUR_API_KEY")
-//                                    .header("cache-control", "no-cache")
-//                                    .header("content-type", "application/x-www-form-urlencoded")
-//                                    .body("sender_id=FSTSMS&language=english&route=qt&numbers=etMobile.getText().toString()&message=39599&variables={#AA#}&variables_values=otp")
-//                                    .asString();
-                            Toast.makeText(MainActivity.this, "OTP Successfully Sent", Toast.LENGTH_SHORT).show();
-                            showDialog();
-                        }
-                        catch (Exception e)
-                        {
-                            Toast.makeText(MainActivity.this, "Error Occurred", Toast.LENGTH_SHORT).show();
-                        }
-
+                        sendOTP();
                     }
                 });
 
         AlertDialog dialog = dialogBuilder.create();
         dialog.show();
+    }
+
+    private void signInUser(PhoneAuthCredential credential) {
+        FirebaseAuth.getInstance().signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(MainActivity.this, "Verified Successfully", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(MainActivity.this, registerFIR.class);
+                            intent.putExtra("mobile", mobile);
+                            startActivity(intent);
+                            finish();
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "Verification Not Successful", Toast.LENGTH_LONG).show();
+                            Log.d(TAG, "onComplete: " + task.getException().getLocalizedMessage());
+                        }
+                    }
+                });
     }
 }
